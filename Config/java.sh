dcc9ac0de9afff7ff784c69fed707d58476f9368
PEDROTUBER44 <77652411+PEDROTUBER44@users.noreply.github.com>
2021-02-28 14:36:23 -0300

    initial commit

---
+++ Config/java.sh
@@ -0,0 +1,6 @@
+su - ;
+echo "deb http://ppa.launchpad.net/linuxuprising/java/ubuntu bionic main" | tee /etc/apt/sources.list.d/linuxuprising-java.list ;
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 73C3DB2A ;
+apt-get update ;
+apt-get install oracle-java11-installer-local -y ;
+exit ;
